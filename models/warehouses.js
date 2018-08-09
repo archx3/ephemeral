4e2d10f5a1cdb79283ba825ba5198ca09f181bad
shaddy shad <mwangishadrack5@gmail.com>
2018-08-09 00:52:50 +0000

    Add warehouse description

--- models/warehouses.js
+++ models/warehouses.js
@@ -1,19 +1,19 @@
 var mongoose = require('mongoose');
 
 var _schemaOptions = {
-   timestamps : { createdAt : 'created_at', updatedAt : 'updated_at' },
-   collection : "warehouses"
+    timestamps: {createdAt: 'created_at', updatedAt: 'updated_at'},
+    collection: "warehouses"
 };
 var _schema = {
-   location    : String,
-   name        : String,
-   area        : Number, //size of warehouse
-   free_space  : Number, //amount of empty space //
-   // msg @Shad rename this to free_space or space_free; empty sounds like a boolean
-   price       : Number,
-   typeOfGoods : String,
-   operator    : { type : mongoose.Schema.Types.ObjectId },
-
+    location: String,
+    name: String,
+    area: Number, //size of warehouse
+    free_space: Number, //amount of empty space //
+    // msg @Shad rename this to free_space or space_free; empty sounds like a boolean
+    price: Number,
+    typeOfGoods: String,
+    operator: {type: mongoose.Schema.Types.ObjectId},
+    description: String
 };
 var warehouseSchema = new mongoose.Schema(_schema, _schemaOptions);
 var Warehouse = mongoose.model('warehouses', warehouseSchema);
